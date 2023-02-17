a29871f622e6a62f0356ded6a2cd0490f50b33ea
0131ryu <131ryuji@gmail.com>
2023-02-17 15:42:30 +0900

    cookie 전달 관련 trust proxy 다시 적용

--- prepare/back/server.js
+++ prepare/back/server.js
@@ -35,6 +35,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(morgan("combined"));
   app.use(hpp());
   app.use(helmet());
+  app.set("trust proxy", 1);
   app.use(
     cors({
       origin: ["http://localhost:3000", "https://engword.shop"],
@@ -56,7 +57,7 @@ passportConfig();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.set("trust proxy", 1);
+
 app.use(
   session({
     saveUninitialized: false,
